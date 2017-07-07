8f5d5685f835bff128e32d17ffd6e08d92455794
pouria <iphone@digitalsunray.com>
2017-07-07 11:55:15 +0200

    implemented, hasUserTappedOnNotification for Android

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -42,6 +42,7 @@ public class FirebasePlugin extends CordovaPlugin {
     protected static final String KEY = "badge";
 
     private static boolean inBackground = true;
+    private static boolean wasTapped = false;
     private static ArrayList<Bundle> notificationStack = null;
     private static CallbackContext notificationCallbackContext;
     private static CallbackContext tokenRefreshCallbackContext;
@@ -54,7 +55,9 @@ public class FirebasePlugin extends CordovaPlugin {
             public void run() {
                 Log.d(TAG, "Starting Firebase plugin");
                 mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
-                if(extras != null && extras.size()>1) {
+                if(extras != null && extras.size() > 1) {
+                    wasTapped = extras.getBoolean("tap", false);
+
                     if (FirebasePlugin.notificationStack == null) {
                         FirebasePlugin.notificationStack = new ArrayList<Bundle>();
                     }
@@ -128,6 +131,11 @@ public class FirebasePlugin extends CordovaPlugin {
             else this.setDefaults(callbackContext, args.getJSONObject(0), null);
             return true;
         }
+        else if (action.equals("hasUserTappedOnNotification")) {
+            hasUserTappedOnNotification(callbackContext);
+            return true;
+        }
+
         return false;
     }
 
@@ -509,6 +517,18 @@ public class FirebasePlugin extends CordovaPlugin {
                 }
             }
         });
+    }
+
+    private void hasUserTappedOnNotification(final CallbackContext callbackContext) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("has_user_tapped_on_notification", wasTapped);
+
+            callbackContext.success(jsonObject);
+        }
+        catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
     }
 
     private static Map<String, Object> defaultsToMap(JSONObject object) throws JSONException {
