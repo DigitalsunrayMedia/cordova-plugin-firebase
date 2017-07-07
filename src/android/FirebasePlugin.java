147bf7d712eb5d8bda1dcba7e24f3c3837f14270
pouria <iphone@digitalsunray.com>
2017-07-07 14:22:26 +0200

    Implemented, hasUserTappedOnNotification function for Android

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -42,6 +42,7 @@ public class FirebasePlugin extends CordovaPlugin {
     protected static final String KEY = "badge";
 
     private static boolean inBackground = true;
+    private static boolean wasTapped = false;
     private static ArrayList<Bundle> notificationStack = null;
     private static CallbackContext notificationCallbackContext;
     private static CallbackContext tokenRefreshCallbackContext;
@@ -50,11 +51,15 @@ public class FirebasePlugin extends CordovaPlugin {
     protected void pluginInitialize() {
         final Context context = this.cordova.getActivity().getApplicationContext();
         final Bundle extras = this.cordova.getActivity().getIntent().getExtras();
+        if(extras != null && extras.size() > 1) {
+            wasTapped = true;
+
         this.cordova.getThreadPool().execute(new Runnable() {
             public void run() {
                 Log.d(TAG, "Starting Firebase plugin");
                 mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
                 if(extras != null && extras.size() > 1) {
+                    wasTapped = extras.getBoolean("tap", false);
 
                     if (FirebasePlugin.notificationStack == null) {
                         FirebasePlugin.notificationStack = new ArrayList<Bundle>();
@@ -517,13 +522,8 @@ public class FirebasePlugin extends CordovaPlugin {
         });
     }
 
-    private void hasUserTappedOnNotification(CallbackContext callbackContext) {
+    private void hasUserTappedOnNotification(final CallbackContext callbackContext) {
         try {
-            boolean wasTapped = false;
-            Bundle extras = this.cordova.getActivity().getIntent().getExtras();
-            if(extras != null)
-                wasTapped = extras.getBoolean("tap", false);
-
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("has_user_tapped_on_notification", wasTapped);
 
